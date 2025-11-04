3765a82f3ff183b1db5979a7faf0e42c7a73559c
Luca Vignola <luca.vignola.00@gmail.com>
2025-11-04 14:09:22 +0100

    fixed bugs for model_to_real_ratio and disagreement metrics

--- ss2r/algorithms/sbsrl/train.py
+++ ss2r/algorithms/sbsrl/train.py
@@ -383,12 +383,13 @@ def train(
             / batch_size
         ),
     )
-    num_model_rollouts = int(
-        model_grad_updates_per_step
-        * batch_size
-        * model_to_real_data_ratio
-        / (1 - model_to_real_data_ratio)
-    )
+    if model_to_real_data_ratio != 1:
+        num_model_rollouts = int(
+            model_grad_updates_per_step
+            * batch_size
+            * model_to_real_data_ratio
+            / (1 - model_to_real_data_ratio)
+        )
     logging.info(f"Num model rollouts: {num_model_rollouts}")
     logging.info(f"Model grad updates per step: {model_grad_updates_per_step}")
     model_replay_buffer = replay_buffers.UniformSamplingQueue(
